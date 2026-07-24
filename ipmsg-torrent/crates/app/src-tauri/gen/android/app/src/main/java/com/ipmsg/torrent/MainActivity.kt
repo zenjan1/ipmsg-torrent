b5506a0e088a4cae5c8c6155eb88527f80ff88f6
zenjan1 <zenjan1@example.com>
2026-07-24 15:54:10 +0800

    Add Android project structure for Tauri build

---
+++ ipmsg-torrent/crates/app/src-tauri/gen/android/app/src/main/java/com/ipmsg/torrent/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ipmsg.torrent
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
